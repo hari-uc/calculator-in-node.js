849ee85fb31924ccf7db4a3d218145a4e9a33898
Hari-uc <hariuc2001@gmail.com>
2021-11-09 12:55:23 +0530

    added new lines in new branch

--- calculator.js
+++ calculator.js
@@ -1,12 +1,26 @@
 const express = require("express");
-const app = express()
+const bodyParser = require("body-parser");
+
+const app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json())
 
 app.get("/" , function(request,response){
     response.sendFile(__dirname + "/index.html");
+});
+
+app.post("/" , function(request,response){
+
+    var num1 = Number(request.body.number1);
+    var num2 = Number(request.body.number2);
+    
 })
+var result = num1 + num2;
 
 
-
+app.send(result);
 
 
 
